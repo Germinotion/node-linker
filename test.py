436ac28363ab15ce046cdb690ecfe4c6e26ef883
Justin <justingermis@gmail.com>
2022-04-05 17:27:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,20 @@ combinations_df = pd.DataFrame (list(combinations(names, 2)))
 combinations_df['y/n'] = ''
 combinations_df['skip?'] = ''
 
+for i in range(len(combinations_df[0][:40])):
+    if len(combinations_df['y/n'][i]) < 1 and len(combinations_df['skip?'][i]) < 1:
+        print(f"Is {combinations_df[0][i]} related to {combinations_df[1][i]}?")
+        y = st.radio('Pick one', ['yes', 'no','skip'])
+        y = input('y/n/skip: ')
+        if y == 'y' or y == 'yes':
+            combinations_df['y/n'][i] = 'y'
+        elif y == 'n' or y == 'no':
+            combinations_df['y/n'][i] = 'n'
+        elif y == 's' or y == 'skip':
+            combinations_df['skip?'][i] = 'y'
+    else:
+        continue
+
 st.dataframe(combinations_df)
 
 my_button = st.button('Push me!')
