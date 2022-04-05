a4a262683f0d5b6ea46a977f43723423e815c66f
Justin <justingermis@gmail.com>
2022-04-05 13:18:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import pandas as pd
+import requests
+
+url = 'https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true'
+r = requests.get(url)
+open('skills.xls', 'wb').write(r.content)
+df = pd.read_excel('skills.xls')
 
 
 
 st.title('Hello World')
-# df = pd.read_csv('./skills.csv')
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true')
+# # df = pd.read_csv('./skills.csv')
+# df = pd.read_csv('https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true')
 
 st.dataframe(df)
 
