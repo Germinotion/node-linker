ab63035c0e814ce21c93581d4a56d9fb9482a5c2
Justin <justingermis@gmail.com>
2022-04-05 23:06:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,38 +3,68 @@ import pandas as pd
 import requests
 from itertools import combinations
 
-st.title('Hello World')
-df = pd.read_csv('https://raw.githubusercontent.com/Germinotion/node-linker/main/skills.csv')
-# st.dataframe(df)
-names = df.name
-combinations_df = pd.DataFrame (list(combinations(names, 2)))
-combinations_df['y/n'] = ''
-combinations_df['skip?'] = ''
+# st.title('Hello World')
 
-st.dataframe(combinations_df)
+st.title('Rolling prompts')
 
+# @st.cache(allow_output_mutation=True)
 @st.experimental_singleton
 def iter_list(x):
 	return iter(list(range(x)))
 
-my_list = iter_list(len(combinations_df[0][:10]))
+x = 10
+my_list = iter_list(x)
 
 button = st.button("Push me")
 
 if button:
 	try:
-# 		num = next(my_list)
-# 		st.write(num)
-# 		if len(combinations_df['y/n'][i]) < 1 and len(combinations_df['skip?'][i]) < 1:
 		num = next(my_list)
 		st.write(num)
-		b = f"Is {combinations_df[0][i]} related to {combinations_df[1][i]}?"
-		st.write(b)
-# 		st.write(num)
 		button = False
 	except:
 		iter_list.clear()
 		button = False
+
+
+# df = pd.read_csv('https://raw.githubusercontent.com/Germinotion/node-linker/main/skills.csv')
+# # st.dataframe(df)
+# names = df.name
+# combinations_df = pd.DataFrame (list(combinations(names, 2)))
+# combinations_df['y/n'] = ''
+# combinations_df['skip?'] = ''
+
+# st.dataframe(combinations_df)
+
+# @st.experimental_singleton
+# def iter_list(x):
+# 	return iter(list(range(x)))
+
+# my_list = iter_list(len(combinations_df[0][:10]))
+
+# button = st.button("Push me")
+
+# if button:
+# 	try:
+# # 		num = next(my_list)
+# # 		st.write(num)
+# # 		if len(combinations_df['y/n'][i]) < 1 and len(combinations_df['skip?'][i]) < 1:
+# 		num = next(my_list)
+# 		st.write(num)
+# 		b = f"Is {combinations_df[0][i]} related to {combinations_df[1][i]}?"
+# 		st.write(b)
+# # 		st.write(num)
+# 		button = False
+# 	except:
+# 		iter_list.clear()
+# 		button = False
+		
+		
+		
+		
+		
+		
+		
 
 # for i in range(len(combinations_df[0][:10])):
 #     if len(combinations_df['y/n'][i]) < 1 and len(combinations_df['skip?'][i]) < 1:
