2687f759bd8165128ffcc0ee84c8484dee989632
Justin <justingermis@gmail.com>
2022-04-05 17:31:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -14,8 +14,8 @@ combinations_df['skip?'] = ''
 for i in range(len(combinations_df[0][:40])):
     if len(combinations_df['y/n'][i]) < 1 and len(combinations_df['skip?'][i]) < 1:
         st.write(f"Is {combinations_df[0][i]} related to {combinations_df[1][i]}?")
-        y = st.radio('Pick one', ['yes', 'no','skip'])
-        y = input('y/n/skip: ')
+#         y = st.radio('Pick one', ['yes', 'no','skip'])
+        y = st.text_input('y/n/skip')
         if y == 'y' or y == 'yes':
             combinations_df['y/n'][i] = 'y'
         elif y == 'n' or y == 'no':
