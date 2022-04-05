1c5213cf466c5865e2c6cf1103c8dfe553617897
Justin <justingermis@gmail.com>
2022-04-05 13:16:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 
 st.title('Hello World')
-df = pd.read_csv('./skills.csv')
+# df = pd.read_csv('./skills.csv')
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true')
 
 st.dataframe(df)
 
@@ -13,3 +14,6 @@ my_button = st.button('Push me!')
 if my_button:
   df.label = "I've been changed!"
   df.to_csv('./skills2.csv')
+
+  
+#   https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true
