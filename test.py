73cf5749e0aebf2b44d3e80043d090d84d696a93
Justin <justingermis@gmail.com>
2022-04-05 13:11:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ st.title('Hello World')
 df = pd.read_csv('./skills.csv')
 
 st.dataframe(df)
+
+my_button = st.button('Push me!')
+
+if my_button:
+  df.label = "I've been changed!"
+  df.to_csv('./skills2.csv')
