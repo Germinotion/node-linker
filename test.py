d4049d0ee3778d36f79f0ad72d6a20c49952e395
Justin <justingermis@gmail.com>
2022-04-05 13:23:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,19 @@ import streamlit as st
 import pandas as pd
 import requests
 
-url = 'https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true'
-r = requests.get(url)
-open('skills.xlsx', 'wb').write(r.content)
-df = pd.read_excel('skills.xlsx')
+# url = 'https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true'
+# r = requests.get(url)
+# open('skills.xlsx', 'wb').write(r.content)
+# df = pd.read_excel('skills.xlsx')
+
+csv = st.file_uploader('Upload a CSV')
 
 
 
 st.title('Hello World')
 # # df = pd.read_csv('./skills.csv')
-# df = pd.read_csv('https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true')
-
+df = pd.read_csv(csv)
+# https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true
 st.dataframe(df)
 
 my_button = st.button('Push me!')
