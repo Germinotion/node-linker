8a84636b2ed40d6ad3f368da5d41990198a7ffed
Justin <justingermis@gmail.com>
2022-04-05 17:35:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ combinations_df = pd.DataFrame (list(combinations(names, 2)))
 combinations_df['y/n'] = ''
 combinations_df['skip?'] = ''
 
+st.dataframe(combinations_df)
+
 for i in range(len(combinations_df[0][:10])):
     if len(combinations_df['y/n'][i]) < 1 and len(combinations_df['skip?'][i]) < 1:
         st.write(f"Is {combinations_df[0][i]} related to {combinations_df[1][i]}?")
@@ -25,13 +27,13 @@ for i in range(len(combinations_df[0][:10])):
     else:
         continue
 
-st.dataframe(combinations_df)
 
-my_button = st.button('Push me!')
 
-if my_button:
-  df.label = "I've been changed!"
-  df.to_csv('./skills2.csv')
+# my_button = st.button('Push me!')
+
+# if my_button:
+#   df.label = "I've been changed!"
+#   df.to_csv('./skills2.csv')
 
   
 #   https://docs.google.com/spreadsheets/d/1R-17pEYYikiOUgCXypFcQIcYAIwL-hFr/edit?usp=sharing&ouid=100405249699989593907&rtpof=true&sd=true
