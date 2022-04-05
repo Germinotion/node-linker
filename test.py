be5dbad33ca56233270ec40eb56f377764543729
Justin <justingermis@gmail.com>
2022-04-05 13:01:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+
+
 st.title('Hello World')
+df = pd.read_excel('https://github.com/Germinotion/node-linker/blob/main/skills.xlsx')
+
+st.dataframe(df)
