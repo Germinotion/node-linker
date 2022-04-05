6ef265cece722aeadbf96ebd55acc331a1f7b9d5
Justin <justingermis@gmail.com>
2022-04-05 17:33:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,17 +11,17 @@ combinations_df = pd.DataFrame (list(combinations(names, 2)))
 combinations_df['y/n'] = ''
 combinations_df['skip?'] = ''
 
-for i in range(len(combinations_df[0][:40])):
+for i in range(len(combinations_df[0][:10])):
     if len(combinations_df['y/n'][i]) < 1 and len(combinations_df['skip?'][i]) < 1:
         st.write(f"Is {combinations_df[0][i]} related to {combinations_df[1][i]}?")
 #         y = st.radio('Pick one', ['yes', 'no','skip'])
-        y = st.text_input('y/n/skip')
-        if y == 'y' or y == 'yes':
-            combinations_df['y/n'][i] = 'y'
-        elif y == 'n' or y == 'no':
-            combinations_df['y/n'][i] = 'n'
-        elif y == 's' or y == 'skip':
-            combinations_df['skip?'][i] = 'y'
+#         y = st.text_input('y/n/skip')
+#         if y == 'y' or y == 'yes':
+#             combinations_df['y/n'][i] = 'y'
+#         elif y == 'n' or y == 'no':
+#             combinations_df['y/n'][i] = 'n'
+#         elif y == 's' or y == 'skip':
+#             combinations_df['skip?'][i] = 'y'
     else:
         continue
 
